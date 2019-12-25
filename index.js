75f7f501fa744a1f1ea23096897044d811da5b5e
nody <nody.email@gmail.com>
2019-12-25 10:09:53 +0500

    initial

---
+++ index.js
@@ -0,0 +1,62 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var numberOfUsers = 0;
+var messageCounter = 0;
+var users = [];
+var messages = [];
+// var msgobj = {[name, message]};
+
+// heroku >>
+var port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+// app.listen(port);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function(){             // << heroku
+  console.log('listening on *: ', port); 
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('update', (updmsg) => {
+    console.log('update (server)' + updmsg);
+    socket.emit('updRes', 'SERVER-SIDE')
+  })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('subscribe', function(msg){
+    console.log('some user just subscribed', msg);
+    socket.emit('new message', 'server-side message');
+  });
+  socket.on('register', function(username){
+    console.log('user registered as ', username);
+    socket.emit('server said', 'You logged as "' + username + '", id: ' + numberOfUsers);
+    users[numberOfUsers] = {id: numberOfUsers, name: username}
+    numberOfUsers++;
+    console.log(users);
+  });
+  socket.on('newmessage', function(msgobj){
+    messages.push({
+      id: messageCounter, 
+      name: msgobj.name, 
+      message: msgobj.message
+    });
+    socket.broadcast.emit('bcast', messages);
+    socket.emit('bcast', messages);
+    console.log(messages);
+    messageCounter++;
+  });
+});
+
